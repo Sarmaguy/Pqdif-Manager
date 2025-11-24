b00616f51a922e96534bf28cd2e605c995e35078
Jura <jura.starcevic@gmail.com>
2025-11-24 11:16:57 +0100

    Added FileVisitor for base data

---
+++ PqdifSaver/Helpers/FileVisitor/BaseFileVisitor.cs
@@ -0,0 +1,18 @@
+using PQDIF_Manager;
+
+public class BaseFileVisitor : AbstractFileVisitor
+{
+    public BaseFileVisitor()
+    {
+        AddRule(
+            filePath => Path.GetExtension(filePath).Equals(".pqd", StringComparison.OrdinalIgnoreCase) && filePath.Contains("10Min_ClassA_PQDIF") && !filePath.Contains("2025-10-26"),//treba rjesiti DST bug, 
+            async filePath =>
+            {
+                Console.WriteLine($"Processing base measurements file: {filePath}");
+                PqdifFile pqdifFile = await PqdifFile.LoadFromFileAsync(filePath);
+                SqlServerMeasurementRepository measurementRepository = new SqlServerMeasurementRepository();
+                await measurementRepository.BulkInsertBaseAsync(pqdifFile);
+                Console.WriteLine($"Finished uploading base measurements from: {filePath}");
+            });
+    }
+}

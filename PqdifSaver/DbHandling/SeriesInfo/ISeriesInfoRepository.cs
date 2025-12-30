bf7ead968433db62913c3bd4977111223f715244
Jura <jura.starcevic@gmail.com>
2025-12-30 17:06:59 +0100

    Removed SeriesInfo

--- PqdifSaver/DbHandling/SeriesInfo/ISeriesInfoRepository.cs
+++
@@ -1,5 +0,0 @@
-public interface ISeriesInfoRepository
-{
-    Task<int> GetSeriesIdAsync(string channelName, string quantityMeasured, string phase, string seriesValueType);
-    Task SaveSeriesInfoAsync(SeriesInfo seriesInfo);
-}
--- PqdifSaver/DbHandling/SeriesInfo/SeriesInfo.cs
+++
@@ -1,9 +0,0 @@
-public class SeriesInfo
-{
-    public int SeriesId { get; set; }
-    public required string ChannelName { get; set; }
-    public required string QuantityMeasured { get; set; }
-    public required string Phase { get; set; }
-    public required string SeriesValueType { get; set; }
-    
-}
